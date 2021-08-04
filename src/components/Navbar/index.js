a7551ca949b56268550f9ecd9e4e3bf5b7323c24
Juan Pablo Castano <jpcastanom@gmail.com>
2021-08-04 12:11:10 -0500

    iteracion 1.1

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+export default function NavBar() {
+  return (
+    <div>
+      <nav className="nav-bar">
+        <h3>WikiCountries</h3>
+      </nav>
+    </div>
+  );
+}
